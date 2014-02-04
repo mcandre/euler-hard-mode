98b5c5d4e779a11802cfacb5733a294fa00d374b
Andrew Pennebaker <apennebaker@42six.com>
2014-02-04 12:49:58 -0500

    c

---
+++ problem5/problem5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "problem5.h"
+
+bool divisible_up_to(long x, long y) {
+	for (long z = 2; z <= y; z++) {
+		if (x % z != 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+long first_divisible_up_to(long y) {
+	long x = 2;
+
+	while (!divisible_up_to(x, y)) {
+		x++;
+	}
+
+	return x;
+}
+
+int main() {
+	printf("%ld\n", first_divisible_up_to(20));
+
+	return 0;
+}
---
+++ problem5/problem5.h
@@ -0,0 +1,12 @@
+#ifndef PROBLEM5_H
+#define PROBLEM5_H
+
+#include <stdbool.h>
+
+bool divisible_up_to(long x, long y);
+
+long first_divisible_up_to(long y);
+
+int main();
+
+#endif
