583c80b83732c746bccc2fa77f5252da102766f2
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-29 16:11:37 -0500

    pep8

--- problem9/problem9.py
+++ problem9/problem9.py
@@ -1,39 +1,47 @@
 #!/usr/bin/env python
 
 import operator
+import functools
 
 #
 # >( Thanks for nothing, Python.
 #
 
+
 def _flatten(lst):
-  for e in lst:
-    if isinstance(e, list):
-      if len(e) > 0:
-        for f in flatten(e):
-          yield f
-    else:
-      yield e
+    for e in lst:
+        if isinstance(e, list):
+            if len(e) > 0:
+                for f in flatten(e):
+                    yield f
+        else:
+            yield e
+
 
 def flatten(lst):
-  return list(_flatten(lst))
+    return list(_flatten(lst))
+
 
 def pythagorean_triple(a, b, c):
-  return a * a + b * b == c * c
+    return a * a + b * b == c * c
+
 
 def main():
-  the_one = flatten(
-    [[[[a, b, c]
-       for a in range(1, b)
-       if a + b + c == 1000 and pythagorean_triple(a, b, c)
-     ] for b in range(1, c)
-    ] for c in range(1, 999)
-   ]
-  )
+    the_one = flatten(
+        [
+            [
+                [
+                    [a, b, c]
+                    for a in range(1, b)
+                    if a + b + c == 1000 and pythagorean_triple(a, b, c)
+                ] for b in range(1, c)
+            ] for c in range(1, 999)
+        ]
+    )
 
-  product = reduce(operator.mul, the_one)
+    product = functools.reduce(operator.mul, the_one)
 
-  print(product)
+    print(product)
 
 if __name__ == "__main__":
-  main()
+    main()
