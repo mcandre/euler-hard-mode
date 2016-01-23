ad01fc231d910ac75fbcbb15fdeb92272601a1d0
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-01-23 11:26:43 -0600

    go indentation

--- problem10/problem10.go
+++ problem10/problem10.go
@@ -3,23 +3,23 @@ package main
 import "fmt"
 
 func prime(n int64) bool {
-  for k := int64(2); k * k <= n; k++ {
-    if n % k == 0 {
-      return false
-    }
-  }
+	for k := int64(2); k * k <= n; k++ {
+		if n % k == 0 {
+			return false
+		}
+	}
 
-  return true
+	return true
 }
 
 func main() {
-  var s = int64(0)
+	var s = int64(0)
 
-  for n := int64(2); n < int64(2000000); n++ {
-    if prime(n) {
-      s += n
-    }
-  }
+	for n := int64(2); n < int64(2000000); n++ {
+		if prime(n) {
+			s += n
+		}
+	}
 
-  fmt.Println(s)
+	fmt.Println(s)
 }
